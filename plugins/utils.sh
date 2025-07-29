68259d3c9c2550ecb4e064f87b032d0e022220c2
Alex Flores <domain@me.com>
2025-07-28 23:01:40 -0500

    fix utils script for tmux compat

--- plugins/utils.sh
+++ plugins/utils.sh
@@ -1,10 +1,10 @@
 
 function linpeas() {
-		url="https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh";
+		url="https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh"
 		(curl -sL "${url}" || wget -O - "${url}") | sh
 }
 
 function chizl() {
-		IFS=: read HOST PORT <<< $ME;
+		IFS=: read HOST PORT <<< $ME
 		ssh -fNT "$@" -p "${PORT}" "${HOST}"
 }
